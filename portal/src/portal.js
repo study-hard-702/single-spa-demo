a9164def10db017afa2d07adec24a35e08a12a56
xiaolianduan <2774894568@qq.com>
2019-08-26 14:46:18 +0800

    update 20190826 duanxiaolian

---
+++ portal/src/portal.js
@@ -0,0 +1,23 @@
+import 'zone.js';
+import * as singleSpa from 'single-spa';
+import {GlobalEventDistributor} from './globalEventDistributor'
+import {loadApp} from './helper'
+
+async function init() {
+  const globalEventDistributor = new GlobalEventDistributor();
+  const loadingPromises = [];
+
+  // app1: The URL "/app1/..." is being redirected to "http://localhost:9001/..." this is done by the webpack proxy (webpack.config.js)
+  loadingPromises.push(loadApp('app1', '/app1', './app1/singleSpaEntry.js', './app1/store.js', globalEventDistributor));
+
+  // app4: The URL "/app4/..." is being redirected to "http://localhost:9004/..." this is done by the webpack proxy (webpack.config.js)
+  loadingPromises.push(loadApp('app4', '/app4', './app4/singleSpaEntry.js', null, null)); // does not have a store, so we pass null
+
+  // wait until all stores are loaded and all apps are registered with singleSpa
+  await Promise.all(loadingPromises);
+
+  singleSpa.start();
+}
+
+init();
+
